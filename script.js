8f60eed0c0719b501aaf03ceb6df6ef0d80faadc
22githrsh <senh2675@gmail.com>
2026-08-10 12:30:47 +0530

    jump working

--- script.js
+++ script.js
@@ -1,65 +1,160 @@
-let mario = document.querySelector(".mario")
-let obstacle = document.querySelector(".obstacle")
-let gameOverBox = document.querySelector(".game-over")
-let button = document.querySelector("button")
-let scoreText = document.querySelector(".score")
-let gameRunning = true
-let marioX = 50
-let marioY = 0
-let obstacleX = 900
-let score = 0
-let isjumping = false
+let mario = document.querySelector(".mario");
+let obstacle = document.querySelector(".obstacle");
+let gameOverBox = document.querySelector(".game-over");
+let button = document.querySelector("button");
+let scoreText = document.querySelector(".score");
+
+let marioX = 50;
+let marioY = 0;
+
+let obstacleX = 800;
+
+let isJumping = false;
+let gameRunning = true;
+
+let score = 0;
 
 
-document.addEventListener("keydown",(e)=>{
-    if(gameRunning === false){
-         return
+
+document.addEventListener("keydown", function (e) {
+
+  if (gameRunning === false) {
+    return;
+  }
+
+
+
+
+  if (e.key === "d" || e.key === "ArrowRight") {
+
+    marioX = marioX + 10;
+
+    if (marioX > 750) {
+      marioX = 750;
     }
-    if(e.key == "d" || e.key === "ArrowRight"){
-        marioX = marioX + 10
-        if(marioX > 750){
-            marioX = 750
+
+    mario.style.left = marioX + "px";
+  }
+
+
+
+
+  if (e.key === "a" || e.key === "ArrowLeft") {
+
+    marioX = marioX - 10;
+
+    if (marioX < 0) {
+      marioX = 0;
+    }
+
+    mario.style.left = marioX + "px";
+  }
+
+
+  // JUMP
+
+  if (
+    e.key === " " ||
+    e.key === "w" ||
+    e.key === "ArrowUp"
+  ) {
+
+    jump();
+
+  }
+
+});
+
+
+
+function jump() {
+
+  if (isJumping === true) {
+    return;
+  }
+
+  isJumping = true;
+
+
+  let jumpUp = setInterval(function () {
+
+    marioY = marioY + 10;
+
+    mario.style.bottom = marioY + "px";
+
+
+    if (marioY >= 130) {
+
+      clearInterval(jumpUp);
+
+
+      let jumpDown = setInterval(function () {
+
+        marioY = marioY - 10;
+
+        mario.style.bottom = marioY + "px";
+
+
+        if (marioY <= 0) {
+
+          marioY = 0;
+
+          mario.style.bottom = "0px";
+
+          clearInterval(jumpDown);
+
+          isJumping = false;
+
         }
-        mario.style.left = marioX  + "px"
-    }
-      if(e.key == "a" || e.key === "Arrowleft"){
-        marioX = marioX - 10
-        if(marioX < 0){
-            marioX = 0
-        }
-        mario.style.left = marioX  + "px"
-    }
-    if(e.key === " " ||
-       e.key === "w" ||
-       e.key === "ArrowUp"
-       
-    ){
-       ///
-    }
 
-
-    function jump(){
-        if(isjumping === true){
-            return
-        }
-        isjumping = true
-        
-        let jumpUp =  setInterval(() => {
-        marioY = marioY + 10
-        mario.style.bottom = marioY + "px"
-
-        if(marioY >= 130){
-            clearInterval(jumpUp)
-
-            let jumpDown = setInterval(() => {
-                marioY = marioY - 10
-                mario.style.bottom = "px"
-                clearInterval(jumpDown)
-                isjumping = false
-            }, 20);
-        }
-        }, 20);
+      }, 20);
 
     }
 
-})
+  }, 20);
+
+}
+
+
+let gameLoop = setInterval(function () {
+
+  
+
+}, 20);
+
+
+
+function gameOver() {
+
+  gameRunning = false;
+
+  gameOverBox.style.display = "flex";
+
+}
+
+
+
+
+button.addEventListener("click", function () {
+
+  marioX = 50;
+  marioY = 0;
+
+  obstacleX = 800;
+
+  score = 0;
+
+
+  mario.style.left = "50px";
+  mario.style.bottom = "0px";
+
+  obstacle.style.left = "800px";
+
+  scoreText.innerHTML = "Score: 0";
+
+  gameOverBox.style.display = "none";
+
+  gameRunning = true;
+
+});
+- [ ] 
